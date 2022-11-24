40ee6c55431c2313104a225f41d99eeab2a2f324
MaximoArceci <98364891+MaximoArceci@users.noreply.github.com>
2022-11-24 08:34:24 -0300

    Finalizacion de Proyecto II

--- final/todasCarreras/dataEntry.py
+++ final/todasCarreras/dataEntry.py
@@ -1,9 +1,9 @@
-import os
+# -*- coding:utf-8 -*-
 import pandas
 
 infoCarreras = []
-
-data = pandas.read_csv("arquitectura&diseño.csv")
+csv_file = 'final/todasCarreras/ejemplo1.csv'
+data = pandas.read_csv(csv_file, encoding="utf-8")
 for i in data.iterrows():
     infoCarreras.append(i[1].array)
 
@@ -11,13 +11,13 @@ for i in data.iterrows():
 #    os.mkdir("UX_templates/paginasTerminadas")
 
 for x in infoCarreras:
-    with open("carrera_template.html", "r") as file:
+    with open("final/todasCarreras/carrera_template.html", "r", encoding="utf-8") as file:
         content = file.read()
-    carrera = [x[2], x[3], "no hay imagen", x[4], x[5], x[6], x[9], x[8], x[9], x[10], "../../css/footerHeader.css", "../template_style.css", x[0]]
-    content = content.replace("{codigo}", x[0])
+    #carrera = [x[2], x[3], "no hay imagen", x[4], x[5], x[6], x[9], x[8], x[9], x[10], "../../css/footerHeader.css", "../template_style.css", x[0]]
+    content = content.replace("{codigo}", str(x[0]))
     content = content.replace("{nombreCarrera}", x[2])
     content = content.replace("{nombreFacultad}", x[3])
-    content = content.replace("{imagen}", x[11])
+    content = content.replace("{imagen}", F"../../imagenes/{x[2].lower()}/{x[11]}.jpg")
     content = content.replace("{duracion}", x[4])
     content = content.replace("{ubicacion}", x[5])
     content = content.replace("{publicoPrivado}", x[6])
@@ -27,15 +27,15 @@ for x in infoCarreras:
     content = content.replace("{linkPag}", x[10])
     content = content.replace("{cssFooterHeader}", "../../../../css/footerHeader.css")
     content = content.replace("{cssTemplate}", "../../../cssCarreras/template_style.css")
+    
 
-    with open(f"{str(x[3]).lower()}Objetivos.txt", "r") as file:
+    with open(f"final/todasCarreras/{str(x[3]).lower()}Objetivos.txt", "r", encoding="utf-8") as file:
         objetivos = file.read()
-        content.replace("{objetivos}", objetivos)
+        content = content.replace("{objetivos}", objetivos)
 
-    with open(f"{str(x[0]).split(sep='_')[0].lower()}Objetivos.txt", "r") as file:
+    with open(f"final/todasCarreras/{str(x[0]).split(sep='_')[0].lower()}Info.txt", "r", encoding="utf-8") as file:
         infoCarrera = file.read()
-        content.replace("{infoCarrera}", infoCarrera)
-
-    with open("arquitectura&diseño/carreras/" + x[2] + "/" + x[0] + ".html", "w") as file:
-        file.write(content)
+        content = content.replace("{infoCarrera}", infoCarrera)
+    with open("final/todasCarreras/arquitectura&diseño/carreras/" + str(x[2]).replace(" ", "") + "/" + x[0] + ".html", "w", encoding="utf-8") as file:
+        file.write(str(content).replace("�", "ñ"))
         print(f"Pagina de {x[0]} creada")
